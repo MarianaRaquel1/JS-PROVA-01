becce46029c8de645341f655ef9dd01a858811e4
Mariana <151977640+MarianaRaquel1@users.noreply.github.com>
2024-06-10 22:03:47 -0300

    Add files via upload

---
+++ prova/app.js
@@ -0,0 +1,9 @@
+let N1 = prompt("Digite a 1º nota")
+let N2 = prompt("Digite a 2º nota")
+let N3 = prompt("Digite a 3º nota")
+const P1 = 1
+const P2 = 2
+const P3 = 3
+let media = (N1*P1+N2*P2+N3*P3)/(P1+P2+P3)
+
+console.log(`Sua média é ${media}`);
